a455b444e4d4c7184555972d58b729e547291bb8
Wang Zishi <ynh.2@outlook.com>
2015-12-15 19:09:58 +0800

    update

--- Dockerfile
+++ Dockerfile
@@ -1,1 +1,4 @@
-FROM node:latest
+# Version: 0.0.1
+FROM node:5.2.0
+MAINTAINER Wang Zishi "ynh.2@outlook.com"
+EXPOSE 3000
--- app.ts
+++ app.ts
@@ -1,20 +1,19 @@
 import * as Koa from 'koa';
 import * as fs from 'fs';
-
 const app = new Koa();
 
 // logger
-app.use(async (ctx, next) => {
-  const start = new Date;
-  await next();
-  const end = new Date;
-  const ms = end.getMilliseconds() - start.getMilliseconds();
-  console.log(`${ctx.method} ${ctx.url} - ${ms}ms`);
+app.use(async(ctx, next) => {
+    const start = new Date;
+    await next();
+    const end = new Date;
+    const ms = end.getMilliseconds() - start.getMilliseconds();
+    console.log(`${ctx.method} ${ctx.url} - ${ms}ms`);
 });
 
 // response
 app.use(ctx => {
-  ctx.body = 'Hello World';
+    ctx.body = 'Hello World';
 });
 
 app.listen(3000);
