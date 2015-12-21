542c2ae8237a762917008a7a7185aad3007f4dcc
Wang Zishi <ynh.2@outlook.com>
2015-12-21 18:01:14 +0800

    Add cluster

--- app.ts
+++ app.ts
@@ -3,29 +3,38 @@ import * as fs from 'fs';
 import * as Router from 'koa-router';
 import * as logger from 'koa-logger';
 import * as log4js from 'log4js';
+import { isMaster, fork, on } from 'cluster'
+import { cpus } from 'os';
 
 import {UserController} from './controllers/user-controller'
-const app = new Koa();
-const user = new UserController();
 log4js.configure({
- appenders: [
-   { type: 'console' },
-   { type: 'file', filename: 'app.log', category: 'app' }
-  ]
+    appenders: [
+        { type: 'console' },
+        { type: 'file', filename: 'app.log', category: 'app' }
+    ]
 })
 const log = log4js.getLogger('app');
 
+if (isMaster) {
+    log.debug(`isMaster: ${isMaster}`);
+    for (var i = 0; i < cpus().length; i++){
+        fork();
+    }
+    // cpus().forEach(() => fork());
+    on('online', (worker, code, signal) => log.info(`worker ${worker.process.pid} online!`))
+    on('exit', (worker, code, signal) => log.info(`worker ${worker.process.pid} died!`));
+} else {
+    const app = new Koa();
+    app.use(logger());
 
-// logger
-app.use(logger());
+    app.use(new UserController().routes());
 
-app.use(user.routes());
+    // response
+    app.use(ctx => {
+        ctx.body = 'Hello World!';
+    });
 
-// response
-app.use(ctx => {
-    ctx.body = 'Hello World!';
-});
+    app.listen(3000);
 
-app.listen(3000);
-
-log.info(`start server on port 3000`);
+    log.info('start server on port 3000');
+}
--- controllers/user-controller.ts
+++ controllers/user-controller.ts
@@ -5,17 +5,21 @@ export class UserController {
     private router: KoaRouter.IRouter;
 
     constructor() {
-        this.router = new Router();
+        this.router = new Router({
+            prefix: "/user"
+        });
+
         this.router.get('/', async (ctx, next) => {
             const start = new Date;
             await next();
+            ctx.body = "We are in '/user'!";
             const end = new Date;
             const ms = end.getMilliseconds() - start.getMilliseconds();
             console.log(`${ctx.method} ${ctx.url} - ${ms}ms`);
         });
     }
 
-    routes(){
+    routes() {
         return this.router.routes()
     }
 }
