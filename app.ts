331dbc435c6e4d8246d3c954d88eceb259b5ef07
Wang Zishi <ynh.2@outlook.com>
2015-12-22 00:36:01 +0800

    Add MongoDB

--- app.ts
+++ app.ts
@@ -1,8 +1,10 @@
 import * as Koa from 'koa';
-import * as fs from 'fs';
 import * as Router from 'koa-router';
 import * as logger from 'koa-logger';
 import * as log4js from 'log4js';
+import * as fs from 'fs';
+import * as assert from 'assert';
+import { MongoClient } from 'mongodb';
 import { isMaster, fork, on } from 'cluster'
 import { cpus } from 'os';
 import { UserController } from './controllers/user-controller'
@@ -16,23 +18,29 @@ log4js.configure({
 const log = log4js.getLogger('app');
 log.info(`Starting application in ${process.env.NODE_ENV} mode.`);
 
+const dbUri = 'mongodb://localhost:27017/test';
+
 if (isMaster && process.env.NODE_ENV == "production") {
     log.info(`Master node ${process.pid} online!`);
     cpus().forEach(() => fork());
     on('online', (worker, code, signal) => log.info(`Slave node ${worker.process.pid} online!`))
     on('exit', (worker, code, signal) => log.info(`Slave node ${worker.process.pid} died!`));
 } else {
-    const app = new Koa();
-    app.use(logger());
+    MongoClient.connect(dbUri, (err, db) => {
+        assert.equal(null, err);
+        log.info(`Connected correctly to server@${dbUri}`);
+        const app = new Koa();
+        app.use(logger());
 
-    app.use(new UserController().routes());
+        app.use(new UserController(db).routes());
 
-    // response
-    app.use(ctx => {
-        ctx.body = 'Hello World!';
+        // response
+        app.use(ctx => {
+            ctx.body = 'Hello World!';
+        });
+
+        app.listen(3000);
+
+        log.info('Server is listening on port 3000...');
     });
-
-    app.listen(3000);
-
-    log.info('Server is listening on port 3000...');
 }
--- controllers/user-controller.ts
+++ controllers/user-controller.ts
@@ -1,22 +1,40 @@
 'use strict'
 import * as Router from 'koa-router';
+import { Db } from 'mongodb';
 
 export class UserController {
     private router: KoaRouter.IRouter;
+    private db: Db
 
-    constructor() {
+    constructor(db: Db) {
+        this.db = db;
+
         this.router = new Router({
-            prefix: "/user"
+            prefix: "/users"
+        });
+
+        this.router.get('/new', async (ctx, next) => {
+            this.db.collection('users', (err, collection) => {
+                collection.insertOne({
+                    user: 'wangzishi',
+                    password: '111'
+                }, (err, result) => {
+                    ctx.body = `result: ${result}`;
+                })
+            });
+            await next();
         });
 
         this.router.get('/', async (ctx, next) => {
-            const start = new Date;
-            await next();
-            ctx.body = "We are in '/user'!";
-            const end = new Date;
-            const ms = end.getMilliseconds() - start.getMilliseconds();
-            console.log(`${ctx.method} ${ctx.url} - ${ms}ms`);
+            this.db.collection('users', (err, collection) => {
+                collection.find((err, result) => {
+                    result.toArray((err, result) => {
+                        ctx.body = result;
+                    })
+                })
+            });
         });
+
     }
 
     routes() {
